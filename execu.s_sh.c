fa5fd96734f15de88d20a7a1bfe1af1ec1bc70ca
steveAmax <asou9i.bambara@gmail.com>
2023-07-15 16:44:29 +0000

    Simple_shell

---
+++ execu.s_sh.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+
+/**
+ * create_process - create a child process which passed the command execute in
+ * @args: array contains the command and its arguments
+ * @env: environ variable
+ * Return: same as execve.
+ */
+
+
+void create_process(char **args, char *env[])
+{
+	int status;
+	pid_t pid = fork();
+
+	if (pid == 0)
+	{
+		if (execve(args[0], args, env) == -1)
+		{
+			perror("Execve(): ");
+		}
+	}
+	else if (pid == -1)
+	{
+		perror("Fork():");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(&status);
+	}
+}
